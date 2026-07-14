9ad3123017803ce23264338c19002c46f7fb0ec0
Matt Grosso <matt.grosso@framebridge.com>
2026-07-13 21:04:11 -0400

    Add a subtitle under the Deck City title (#4)

--- src/game/scenes/MainScene.js
+++ src/game/scenes/MainScene.js
@@ -6,10 +6,16 @@ export default class MainScene extends Phaser.Scene {
   }
 
   create () {
-    this.add.text(this.scale.width / 2, this.scale.height / 2, 'Deck City', {
+    this.add.text(this.scale.width / 2, this.scale.height / 2 - 20, 'Deck City', {
       fontFamily: 'sans-serif',
       fontSize: '48px',
       color: '#ffffff'
+    }).setOrigin(0.5)
+
+    this.add.text(this.scale.width / 2, this.scale.height / 2 + 40, 'a deck-building city builder', {
+      fontFamily: 'sans-serif',
+      fontSize: '18px',
+      color: '#9aa4b2'
     }).setOrigin(0.5)
   }
 }
